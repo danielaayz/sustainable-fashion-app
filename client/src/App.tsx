cc7c5b8f30aa0719028cd407118f6db0f880eb42
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-01-10 10:16:17 +0100

    fixed itemmodal link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { useState } from "react";
+import { Item } from "./types/ItemTypes";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import Profile from "./components/Profile";
@@ -10,17 +12,24 @@ import Contact from "./components/Contact";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import WardrobePage from "./components/WardrobePage";
-import ItemDetailModal from "./components/ItemModal";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ItemDetailModal from "./components/ItemModal";
 
 function App() {
+   const [selectedItem, setSelectedItem] = useState<Item | null>(null);
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
+   const handleCloseModal = () => {
+      setIsModalOpen(false);
+      setSelectedItem(null);
+   };
+
    return (
       <div className="flex flex-col min-h-screen">
          <Header />
          <div className="flex-grow">
             <Routes>
                <Route index element={<Homepage />} />
-               <Route index element={<ItemDetailModal />} />
                <Route path="/profile" element={<Profile />} />
                <Route path="/add-items" element={<AddItemForm />} />
                <Route path="/add-items/:id" element={<AddItemForm />} />
@@ -32,6 +41,14 @@ function App() {
                <Route path="/register" element={<Register />} />
                <Route path="/login" element={<Login />} />
             </Routes>
+
+            {selectedItem && (
+               <ItemDetailModal
+                  isOpen={isModalOpen}
+                  onClose={handleCloseModal}
+                  item={selectedItem}
+               />
+            )}
          </div>
          <Footer />
       </div>
